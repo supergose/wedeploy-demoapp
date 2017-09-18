906248f657a2d78b4df36e984009343b77552def
Marcello <marcello.tosetti@gmail.com>
2017-09-18 17:26:39 +0200

    spring boot api

---
+++ java/src/main/java/com/sample/soggettoposizioneinformativa/SoggettoPosizioneinformativaApplication.java
@@ -0,0 +1,12 @@
+package com.sample.soggettoposizioneinformativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoggettoPosizioneinformativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoggettoPosizioneinformativaApplication.class, args);
+	}
+}
---
+++ java/src/test/java/com/sample/soggettoposizioneinformativa/SoggettoPosizioneinformativaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.soggettoposizioneinformativa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoggettoPosizioneinformativaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
